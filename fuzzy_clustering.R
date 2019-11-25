cda1cedd4fc15b39b2fee2db721c4e66b41b7d4a
learner-wxy <superwxy@aliyun.com>
2019-11-25 20:49:39 +0800

    模糊聚类

---
+++ fuzzy_clustering.R
@@ -0,0 +1,14 @@
+# 模糊聚类法
+library(foreign)
+library(cluster)
+
+mydata<-read.spss("./3.7.1Asia.sav") #读取E盘下变量名为3.7.1Asia.sav的SPSS原始数据
+mydata
+X<-as.data.frame(mydata)  #转换数据格式
+X
+Z<-data.frame(scale(X[,2:7]),row.names=X[,1]) #对数据进行标准化，并将各行命名为相应的国家
+Z
+fresult<-fanny(Z,3)#聚成三类
+fresult
+summary(fresult)
+plot(fresult)#输出分类图，与侧影图（类似冰柱图1:5|0.60，代表第一类包含5个样品，并且这5个样品平均轮廓宽度为0.60）
